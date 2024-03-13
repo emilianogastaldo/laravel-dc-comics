9d86f45a1ddaca5dcbbab0f25991cdbd232afbf8
emilianogastaldo <emiliano0303@gmail.com>
2024-03-13 16:56:10 +0100

    added links

---
+++ config/footer_icons.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    ["name" => 'Facebook icon', "img" => 'footer-facebook.png', "href" => "#"],
+    ["name" => 'Twitter icon', "img" => 'footer-twitter.png', "href" => "#"],
+    ["name" => 'Youtube icon', "img" => 'footer-youtube.png', "href" => "#"],
+    ["name" => 'Pinterest icon', "img" => 'footer-pinterest.png', "href" => "#"],
+    ["name" => 'Periscope icon', "img" => 'footer-periscope.png', "href" => "#"]
+];
